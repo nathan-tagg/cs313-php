1099767ab65aac8f5b087ffa03432188f191048b
Nathan Tagg <taggnathan@gmail.com>
2018-09-29 20:46:04 -0500

    working

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
 <?php
 
-  phpinfo();
+echo "<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="./siameseFightingFish.css">
+  </head>
+  <body>
+    <h1>Homepage</h1>
+  </body>
+</html>";
 
 ?>
