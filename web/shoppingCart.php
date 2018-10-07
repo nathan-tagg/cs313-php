120b1fbbfef1f89d363e80f85f27b299c78f40c5
Nathan Tagg <taggnathan@gmail.com>
2018-10-07 11:36:48 -0600

    working

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -6,17 +6,17 @@ $itemFour  = htmlspecialchars($_POST["numberOfItemFour"]);
 $itemFive  = htmlspecialchars($_POST["numberOfItemFive"]);
 $itemSix   = htmlspecialchars($_POST["numberOfItemSix"]);
 
-if (isset($_COOKIE['one']))   { $_COOKIE['one'] += $itemOne; }
+if (isset($_COOKIE['one']))   { setcookie(one, $_COOKIE['one'] + $itemOne, time() + 2147483647); }
 else { setcookie(one, $itemOne, time() + 2147483647); }
-if (isset($_COOKIE['two']))   { $_COOKIE['two'] += $itemTwo; }
+if (isset($_COOKIE['two']))   { setcookie(two, $_COOKIE['two'] + $itemTwo, time() + 2147483647); }
 else { setcookie(two, $itemTwo, time() + 2147483647); }
-if (isset($_COOKIE['three'])) { $_COOKIE['three'] += $itemThree; }
+if (isset($_COOKIE['three'])) { setcookie(three, $_COOKIE['three'] + $itemThree, time() + 2147483647); }
 else { setcookie(three, $itemThree, time() + 2147483647); }
-if (isset($_COOKIE['four']))  { $_COOKIE['four'] += $itemFour; }
+if (isset($_COOKIE['four']))  { setcookie(four, $_COOKIE['four'] + $itemFour, time() + 2147483647); }
 else { setcookie(four, $itemFour, time() + 2147483647); }
-if (isset($_COOKIE['five']))  { $_COOKIE['five'] += $itemFive; }
+if (isset($_COOKIE['five']))  { setcookie(five, $_COOKIE['five'] + $itemFive, time() + 2147483647); }
 else { setcookie(five, $itemFive, time() + 2147483647); }
-if (isset($_COOKIE['six']))   { $_COOKIE['six'] += $itemSix; }
+if (isset($_COOKIE['six']))   { setcookie(six, $_COOKIE['six'] + $itemSix, time() + 2147483647); }
 else { setcookie(six, $itemSix, time() + 2147483647); }
 ?>
 
