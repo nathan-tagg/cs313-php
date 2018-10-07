8a504d93110d4a0027445f52c82a8b5d356f34ce
Nathan Tagg <taggnathan@gmail.com>
2018-10-07 15:23:58 -0600

    working

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -14,18 +14,18 @@
     $itemFive  = htmlspecialchars($_POST["numberOfItemFive"]);
     $itemSix   = htmlspecialchars($_POST["numberOfItemSix"]);
 
-    if (isset($_COOKIE['one']))   { setcookie(one, $_COOKIE['one'] + $itemOne, time() + 2147483647); $itemOne = $_COOKIE['one']; }
-    else { setcookie(one, $itemOne, time() + 2147483647); $itemOne = $_COOKIE['one']; }
-    if (isset($_COOKIE['two']))   { setcookie(two, $_COOKIE['two'] + $itemTwo, time() + 2147483647); $itemTwo = $_COOKIE['two']; }
-    else { setcookie(two, $itemTwo, time() + 2147483647); $itemTwo = $_COOKIE['two']; }
-    if (isset($_COOKIE['three'])) { setcookie(three, $_COOKIE['three'] + $itemThree, time() + 2147483647); $itemThree = $_COOKIE['three']; }
-    else { setcookie(three, $itemThree, time() + 2147483647); $itemThree = $_COOKIE['three']; }
-    if (isset($_COOKIE['four']))  { setcookie(four, $_COOKIE['four'] + $itemFour, time() + 2147483647); $itemFour = $_COOKIE['four']; }
-    else { setcookie(four, $itemFour, time() + 2147483647); $itemFour = $_COOKIE['four']; }
-    if (isset($_COOKIE['five']))  { setcookie(five, $_COOKIE['five'] + $itemFive, time() + 2147483647); $itemFive = $_COOKIE['five']; }
-    else { setcookie(five, $itemFive, time() + 2147483647); $itemFive = $_COOKIE['five']; }
-    if (isset($_COOKIE['six']))   { setcookie(six, $_COOKIE['six'] + $itemSix, time() + 2147483647); $itemSix = $_COOKIE['six']; }
-    else { setcookie(six, $itemSix, time() + 2147483647); $itemSix = $_COOKIE['six']; }
+    if (isset($_COOKIE['one']))   { setcookie(one, $_COOKIE['one'] + $itemOne, time() + 2147483647); $itemOne += $_COOKIE['one']; }
+    else { setcookie(one, $itemOne, time() + 2147483647); $itemOne += $_COOKIE['one']; }
+    if (isset($_COOKIE['two']))   { setcookie(two, $_COOKIE['two'] + $itemTwo, time() + 2147483647); $itemTwo += $_COOKIE['two']; }
+    else { setcookie(two, $itemTwo, time() + 2147483647); $itemTwo += $_COOKIE['two']; }
+    if (isset($_COOKIE['three'])) { setcookie(three, $_COOKIE['three'] + $itemThree, time() + 2147483647); $itemThree += $_COOKIE['three']; }
+    else { setcookie(three, $itemThree, time() + 2147483647); $itemThree += $_COOKIE['three']; }
+    if (isset($_COOKIE['four']))  { setcookie(four, $_COOKIE['four'] + $itemFour, time() + 2147483647); $itemFour += $_COOKIE['four']; }
+    else { setcookie(four, $itemFour, time() + 2147483647); $itemFour += $_COOKIE['four']; }
+    if (isset($_COOKIE['five']))  { setcookie(five, $_COOKIE['five'] + $itemFive, time() + 2147483647); $itemFive += $_COOKIE['five']; }
+    else { setcookie(five, $itemFive, time() + 2147483647); $itemFive += $_COOKIE['five']; }
+    if (isset($_COOKIE['six']))   { setcookie(six, $_COOKIE['six'] + $itemSix, time() + 2147483647); $itemSix += $_COOKIE['six']; }
+    else { setcookie(six, $itemSix, time() + 2147483647); $itemSix += $_COOKIE['six']; }
     ?>
 
     <?if ($itemOne) echo $itemOne . " * one";?>
