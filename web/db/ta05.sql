2ae1f36913196428deeae0f4c99ded571672b43f
Nathan Tagg <taggnathan@gmail.com>
2018-10-15 16:19:12 -0600

    Add ta05

--- web/db/ta05.sql
+++ web/db/ta05.sql
@@ -8,29 +8,33 @@ CREATE TABLE SCRIPTURE
 CREATE SEQUENCE scriptures_s1 START WITH 1001;
 
 INSERT INTO SCRIPTURES
-( -- scripture id
-, -- book
-, -- chapter
-, -- verse
-, -- content )
+( scriptures_s1.nextval() -- scripture id
+, 'John'-- book
+, 1     -- chapter
+, 5     -- verse
+, 'And the light shineth in darkness; and the darkness comprehended it not.' -- content
+);
 
 INSERT INTO SCRIPTURES
-( -- scripture id
-, -- book
-, -- chapter
-, -- verse
-, -- content )
+( scriptures_s1.nextval() -- scripture id
+, 'Doctrine and Covenants'-- book
+, 88 -- chapter
+, 49 -- verse
+, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.'-- content
+);
 
 INSERT INTO SCRIPTURES
-( -- scripture id
-, -- book
-, -- chapter
-, -- verse
-, -- content )
+( scriptures_s1.nextval() -- scripture id
+, 'Doctrine and Covenants'-- book
+, 93-- chapter
+, 28-- verse
+, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.'-- content
+);
 
 INSERT INTO SCRIPTURES
-( -- scripture id
-, -- book
-, -- chapter
-, -- verse
-, -- content )
+( scriptures_s1.nextval()-- scripture id
+, 'Mosiah'-- book
+, 16-- chapter
+, 9-- verse
+, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.'-- content
+);
--- web/ta05.php
+++ web/ta05.php
@@ -8,23 +8,30 @@
 
     <h1>Ta 05</h1>
 
-    <?
-      $conn = pg_pconnect("dbname=ddgliuko4bnn30");
-      if (!$conn) {
-        echo "ERROR: unable to connect to database\n";
-        exit;
-      }
+    <?php
 
-      $result = pg_query($conn, "SELECT");
-        if (!$result) {
-          echo "ERROR: invalid query\n";
-          exit;
+      $host = 'ec2-54-235-73-241.compute-1.amazonaws.com';
+      $db = 'ddgliuko4bnn30';
+      $username = 'lrjdfhijghkjgr';
+      $password = '022793f308a02ea702614c5a2d20ac4d2ab20840c3c722847c870416949c3e2f';
+
+
+      $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+
+      try{
+        // create a PostgreSQL database connection
+        $conn = new PDO($dsn);
+
+        // display a message if connected to the PostgreSQL successfully
+        if($conn){
+          echo "Connected to the <strong>$db</strong> database successfully!";
         }
-
-      while ($row = pg_fetch_row($result)) {
-        
+      }catch (PDOException $e){
+        // report error message
+        echo $e->getMessage();
       }
-    ?>
+
+?>
 
   </body>
 </html>
