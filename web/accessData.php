8d1fe57e7a4dedcd99077bd37a6f3a7e6d31693a
Nathan Tagg <taggnathan@gmail.com>
2018-10-20 19:17:47 -0600

    populate database

--- web/accessData.php
+++ web/accessData.php
@@ -10,13 +10,14 @@
 
     <?
     $host = 'ec2-54-235-73-241.compute-1.amazonaws.com';
-    $db = 'ddgliuko4bnn30';
+    $dbname = 'ddgliuko4bnn30';
     $username = 'lrjdfhijghkjgr';
     $password = '022793f308a02ea702614c5a2d20ac4d2ab20840c3c722847c870416949c3e2f';
     try
     {
-      $db = new PDO("pgsql:host=$host;port=5432;dbname=$db", $username, $password);
+      $db = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $username, $password);
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      if($db){echo "Connected to the <strong>$dbname</strong> database successfully!";}
     }
     catch (PDOException $ex)
     {
@@ -24,7 +25,7 @@
       die();
     }
 
-    
+
     ?>
 
   </body>
