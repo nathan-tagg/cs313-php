bf8c6f32b96626dd0af9a23e16dee5abd35ea97e
Nathan Tagg <taggnathan@gmail.com>
2018-10-28 00:33:19 -0600

    working...

---
+++ web/newContact.php
@@ -0,0 +1,22 @@
+<?php
+$name_first = htmlspecialchars($_POST["firstName"]);
+$name_middle = htmlspecialchars($_POST["middleName"]);
+$name_last = htmlspecialchars($_POST["lastName"]);
+$relationship = htmlspecialchars($_POST["relationship"]);
+$birthday = htmlspecialchars($_POST["birthday"]);
+$picture = htmlspecialchars($_POST["image"]);
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Contact added</h1>
+
+  </body>
+</html>
