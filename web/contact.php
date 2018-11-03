8521939b46b0b1e453c6a33b6cce304f7b3a34db
Nathan Tagg <taggnathan@gmail.com>
2018-11-03 15:49:41 -0600

    working...

--- web/contact.php
+++ web/contact.php
@@ -1,3 +1,9 @@
+<?php
+
+$person_id = htmlspecialchars($_POST["person_id"]);
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
--- web/newContact.php
+++ web/newContact.php
@@ -1,4 +1,5 @@
 <?php
+//TODO: if added functionality of logging in, change the whodunit to the current user. that is all.
 $name_first = htmlspecialchars($_POST["firstName"]);
 $name_middle = htmlspecialchars($_POST["middleName"]);
 $name_last = htmlspecialchars($_POST["lastName"]);
@@ -21,6 +22,9 @@ switch (htmlspecialchars($_POST["relationship"])) {
 $birthday = htmlspecialchars($_POST["birthday"]);
 $picture = htmlspecialchars($_POST["image"]);
 
+$numberOfPhoneNumbers = (int)htmlspecialchars($_POST['numberOfPhoneNumbers']);
+$numberOfAddresses = (int)htmlspecialchars($_POST['numberOfAddresses']);
+$numberOfEmails = (int)htmlspecialchars($_POST['numberOfEmails']);
 
 $host = 'ec2-54-235-73-241.compute-1.amazonaws.com';
 $dbname = 'ddgliuko4bnn30';
@@ -38,7 +42,7 @@ catch (PDOException $ex)
 }
 
 
-
+// Insert the peoples
 $query = 'INSERT INTO PERSON(SELECT nextval(\'person_s1\'), :relationship, :name_first, :name_middle, :name_last, :birthday, :picture, (SELECT system_user_id FROM system_user WHERE system_user_name = \'Nathan\'))';
 
   $statement = $db->prepare($query);
@@ -50,6 +54,37 @@ $query = 'INSERT INTO PERSON(SELECT nextval(\'person_s1\'), :relationship, :name
   $statement->bindValue(':birthday', $birthday);
   $statement->bindValue(':picture', $picture);
   $statement->execute();
+
+// Insert the telephones
+
+for ($i=1; $i <= $numberOfPhoneNumbers; $i++) {
+
+
+  $telephone_type = htmlspecialchars($_POST['telephone_type' . $i]);
+  $country_code = htmlspecialchars($_POST['countryCode' . $i]);
+  $area_code = htmlspecialchars($_POST['areaCode' . $i]);
+  $telephone_number = htmlspecialchars($_POST['phoneNumber' . $i]);
+
+  $query = "INSERT INTO TELEPHONE
+  ( SELECT nextval('telephone_s1')
+  , :telephone_type -- Type
+  , (SELECT currval('person_s1'))
+  , :country_code -- country_code
+  , :area_code -- area_code
+  , :telephone_number -- telephone_number
+  , (SELECT system_user_id
+     FROM system_user
+     WHERE system_user_name = 'Nathan'))";
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':telephone_type', $telephone_type);
+    $statement->bindValue(':name_first', $country_code);
+    $statement->bindValue(':name_middle', $area_code);
+    $statement->bindValue(':name_last', $telephone_number);
+
+    $statement->execute();
+}
 
  ?>
 
