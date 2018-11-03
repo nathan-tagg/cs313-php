4954d836da11316b900a1297072880e464051356
Nathan Tagg <taggnathan@gmail.com>
2018-11-03 16:16:50 -0600

    working...

--- web/newContact.php
+++ web/newContact.php
@@ -79,14 +79,10 @@ for ($i=1; $i <= $numberOfPhoneNumbers; $i++) {
   $area_code = htmlspecialchars($_POST['areaCode' . $i]);
   $telephone_number = htmlspecialchars($_POST['phoneNumber' . $i]);
 
-  $query = 'INSERT INTO TELEPHONE(SELECT nextval(\'telephone_s1\'), :telephone_type, (SELECT currval(\'person_s1\')), :country_code, :area_code, :telephone_number, (SELECT system_user_id FROM system_user WHERE system_user_name = \'Nathan\'))';
+  $query = '';
 
     $statement = $db->prepare($query);
 
-    $statement->bindValue(':telephone_type', $telephone_type);
-    $statement->bindValue(':country_code', $country_code);
-    $statement->bindValue(':area_code', $area_code);
-    $statement->bindValue(':telephone_number', $telephone_number);
 
     $statement->execute();
 }
