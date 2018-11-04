8c7abc63f849e249084db68c605b4fd53f722509
Nathan Tagg <taggnathan@gmail.com>
2018-11-04 00:13:12 -0600

    working...

--- web/contact.php
+++ web/contact.php
@@ -1,6 +1,25 @@
 <?php
 
+$host = 'ec2-54-235-73-241.compute-1.amazonaws.com';
+$dbname = 'ddgliuko4bnn30';
+$username = 'lrjdfhijghkjgr';
+$password = '022793f308a02ea702614c5a2d20ac4d2ab20840c3c722847c870416949c3e2f';
+try
+{
+  $db = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $username, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
 $person_id = htmlspecialchars($_POST["person_id"]);
+
+foreach ($db->query('SELECT name_first, name_last, picture_url FROM PERSON WHERE person_id  =' . $person_id) as $row) {
+$picture = $row['picture_url'];
+}
 
  ?>
 
@@ -9,8 +28,33 @@ $person_id = htmlspecialchars($_POST["person_id"]);
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="./addressBook.css">
   </head>
   <body>
+
+    <blockquote>
+      <div class="person">
+        <?echo '<img src="' . $picture . '" alt="">';?>
+      </div>
+    </blockquote>
+
+    <blockquote>
+      <div class="telephone">
+
+      </div>
+    </blockquote>
+
+    <blockquote>
+      <div class="address">
+
+      </div>
+    </blockquote>
+
+    <blockquote>
+      <div class="email">
+
+      </div>
+    </blockquote>
 
   </body>
 </html>
