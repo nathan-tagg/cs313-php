9f6ef3794128ffcde6594c3f9d4c7332679ad88c
Nathan Tagg <taggnathan@gmail.com>
2018-11-03 19:42:19 -0600

    working...

--- web/newContact.php
+++ web/newContact.php
@@ -77,13 +77,13 @@ for ($i=1; $i <= $numberOfPhoneNumbers; $i++) {
   $country_code = (int)htmlspecialchars($_POST['countryCode' . $i]);
   $area_code = (int)htmlspecialchars($_POST['areaCode' . $i]);
   $telephone_number = (int)htmlspecialchars($_POST['phoneNumber' . $i]);
-  $query = 'INSERT INTO TELEPHONE(SELECT nextval(\'telephone_s1\'), :telephone_type, (SELECT currval(\'person_s1\')), :country_code, :area_code, :telephone_number, (SELECT system_user_id FROM system_user WHERE system_user_name = \'Nathan\'))';
+/*  $query = 'INSERT INTO TELEPHONE(SELECT nextval(\'telephone_s1\'), :telephone_type, (SELECT currval(\'person_s1\')), :country_code, :area_code, :telephone_number, (SELECT system_user_id FROM system_user WHERE system_user_name = \'Nathan\'))';
     $statement = $db->prepare($query);
     $statement->bindValue(':telephone_type', $telephone_type);
     $statement->bindValue(':country_code', $country_code);
     $statement->bindValue(':area_code', $area_code);
     $statement->bindValue(':telephone_number', $telephone_number);
-    $statement->execute();
+    $statement->execute(); */
 }
  ?>
 
